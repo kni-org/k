75b48200c3c8d7d638ab961644e0bd54c0456bc4
Niranjan Kumar K <kni.org.com@gmail.com>
2026-03-28 15:47:30 +0530

    Add files via upload

---
+++ termux_set_up.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "----------------------------------------"
+echo " Welcome To K Programming Setup "
+echo " Developer : Niranjan Kumar K "
+echo " Version   : 1.0 "
+echo "----------------------------------------"
+echo "Setting up..."
+
+mkdir -p $PREFIX/bin
+
+curl -L https://kni-org.github.io/k/k -o $PREFIX/bin/k
+
+chmod +x $PREFIX/bin/k
+
+echo ""
+echo "----------------------------------------"
+echo " K Program Installed Successfully ! "
+echo "----------------------------------------"
+echo ""
+echo "verify : k --version "
