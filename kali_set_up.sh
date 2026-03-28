4c5cc0a338080f8ba0a6bd4265ef3476781d84e9
Niranjan Kumar K <kni.org.com@gmail.com>
2026-03-28 15:54:29 +0530

    Add files via upload

---
+++ kali_set_up.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "----------------------------------------"
+echo " Welcome To K Programming Setup "
+echo " Developer : Niranjan Kumar K "
+echo " Version   : 1.0 "
+echo "----------------------------------------"
+echo "Setting up..."
+
+sudo curl -L https://kni-org.github.io/k/k -o /usr/local/bin/k
+
+sudo chmod +x /usr/local/bin/k
+
+echo ""
+echo "----------------------------------------"
+echo " K Program Installed Successfully ! "
+echo "----------------------------------------"
+echo ""
+echo "verify : k --version "
