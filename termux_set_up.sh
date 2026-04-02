66e2ed5b99b22a31ade1f60b06d2e3b0257d1743
Niranjan Kumar K <kni.org.com@gmail.com>
2026-04-02 12:29:29 +0530

    termux_set_up.sh

--- termux_set_up.sh
+++ termux_set_up.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "----------------------------------------"
-echo " Welcome To K Programming Setup "
-echo " Developer : Niranjan Kumar K "
-echo " Version   : 1.0 "
-echo "----------------------------------------"
-echo "Setting up..."
+echo -e "\033[34m----------------------------------------\033[0m"
+echo -e "\033[32m Welcome To \033[33mK Programming Setup \033[0m"
+echo -e "\033[36m Developer : \033[35mNiranjan Kumar K \033[0m"
+echo -e "\033[36m Version   : \033[31m1.0 \033[0m"
+echo -e "\033[34m----------------------------------------\033[0m"
+echo -e "\033[33mSetting up...\033[0m"
 
 mkdir -p $PREFIX/bin
 
@@ -14,10 +14,10 @@ curl -L https://kni-org.github.io/k/k_termux -o $PREFIX/bin/k
 chmod +x $PREFIX/bin/k
 
 echo ""
-echo "----------------------------------------"
-echo " K Program Installed Successfully ! "
-echo "----------------------------------------"
+echo -e "\033[34m----------------------------------------\033[0m"
+echo -e "\033[32m K Program Installed Successfully ! \033[0m"
+echo -e "\033[34m----------------------------------------\033[0m"
 echo ""
-echo "verify : k --version "
+echo -e "\033[33mverify : \033[36mk --version \033[0m"
 
 rm termux_set_up.sh
