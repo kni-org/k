881c198151bdf12097dab5446f3ce47e2186cdb4
Niranjan Kumar K <kni.org.com@gmail.com>
2026-04-02 12:31:05 +0530

    win_set_up.bat

--- win_set_up.bat
+++ win_set_up.bat
@@ -1,9 +1,11 @@
 @echo off
+:: Enable ANSI escape codes in Windows 10+ cmd
+for /f "delims=" %%A in ('echo prompt $E ^| cmd') do set "ESC=%%A"
 
-echo -- Welcome To K Programming Set Up      --
-echo -- Developer : Niranjan Kumar K         --
-echo -- Version   : 1.0                      --
-echo -- setting up......................     --
+echo %ESC%[34m-- Welcome To %ESC%[33mK Programming Set Up      %ESC%[0m--
+echo %ESC%[32m-- Developer : %ESC%[36mNiranjan Kumar K         %ESC%[0m--
+echo %ESC%[32m-- Version   : %ESC%[31m1.0                      %ESC%[0m--
+echo %ESC%[33m-- setting up......................     %ESC%[0m--
 
 if not exist C:\k_programming mkdir C:\k_programming
 
@@ -13,6 +15,6 @@ for /f "tokens=2*" %%A in ('reg query HKCU\Environment /v PATH') do set OLDPATH=
 setx PATH "%OLDPATH%;C:\k_programming"
 
 echo.
-echo -- Successfully installed k programming --
-echo -- Restart CMD and run: k --version --
+echo %ESC%[32m-- Successfully installed k programming --%ESC%[0m
+echo %ESC%[33m-- Restart CMD and run: %ESC%[36mk --version --%ESC%[0m
 del win_set_up.bat
