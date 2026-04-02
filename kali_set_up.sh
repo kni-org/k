e72057704d843d7ae39f6d0d4e5e9426f5c132ef
Niranjan Kumar K <kni.org.com@gmail.com>
2026-04-02 12:28:32 +0530

    kali_set_up.sh

--- kali_set_up.sh
+++ kali_set_up.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-echo "----------------------------------------"
-echo " Welcome To K Programming Setup "
-echo " Developer : Niranjan Kumar K "
-echo " Version   : 1.0 "
-echo "----------------------------------------"
-echo "Setting up..."
+echo -e "\033[34m----------------------------------------\033[0m"
+echo -e "\033[32m Welcome To \033[33mK Programming Setup \033[0m"
+echo -e "\033[36m Developer : \033[35mNiranjan Kumar K \033[0m"
+echo -e "\033[36m Version   : \033[31m1.0 \033[0m"
+echo -e "\033[34m----------------------------------------\033[0m"
+echo -e "\033[33mSetting up...\033[0m"
 
 sudo curl -L https://kni-org.github.io/k/k -o /usr/local/bin/k
 
 sudo chmod +x /usr/local/bin/k
 
 echo ""
-echo "----------------------------------------"
-echo " K Program Installed Successfully ! "
-echo "----------------------------------------"
+echo -e "\033[34m----------------------------------------\033[0m"
+echo -e "\033[32m K Program Installed Successfully ! \033[0m"
+echo -e "\033[34m----------------------------------------\033[0m"
 echo ""
-echo "verify : k --version "
+echo -e "\033[33mverify : \033[36mk --version \033[0m"
 rm kali_set_up.sh
