ef284d7871727b88d897f34f1d217a6fcac17c90
Niranjan Kumar K <kni.org.com@gmail.com>
2026-04-16 11:11:22 +0530

    termux_set_up.sh

--- termux_set_up.sh
+++ termux_set_up.sh
@@ -7,11 +7,21 @@ echo -e "\033[36m Version   : \033[31m1.0 \033[0m"
 echo -e "\033[34m----------------------------------------\033[0m"
 echo -e "\033[33mSetting up...\033[0m"
 
-mkdir -p $PREFIX/bin
+pkg install unzip curl -y >/dev/null 2>&1
 
-curl -L https://kni-org.github.io/k/k_termux -o $PREFIX/bin/k
+mkdir -p $PREFIX/bin/k_programming >/dev/null 2>&1
 
-chmod +x $PREFIX/bin/k
+curl -L https://kni-org.github.io/k/k.zip -o k.zip >/dev/null 2>&1
+
+unzip -o k.zip -d $PREFIX/bin/k_programming >/dev/null 2>&1
+
+chmod +x $PREFIX/bin/k_programming/k >/dev/null 2>&1
+
+echo '#!/bin/bash
+DIR=$PREFIX/bin/k_programming
+LD_LIBRARY_PATH=$DIR:$PREFIX/lib $DIR/k "$@" 2>/dev/null' > $PREFIX/bin/k
+
+chmod +x $PREFIX/bin/k >/dev/null 2>&1
 
 echo ""
 echo -e "\033[34m----------------------------------------\033[0m"
@@ -20,4 +30,5 @@ echo -e "\033[34m----------------------------------------\033[0m"
 echo ""
 echo -e "\033[33mverify : \033[36mk --version \033[0m"
 
-rm termux_set_up.sh
+rm k.zip >/dev/null 2>&1
+rm termux_set_up.sh >/dev/null 2>&1
